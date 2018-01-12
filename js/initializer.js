4cf0305460420bae9e2dcaab353e6b1b1a93f231
Kuan Butts <kuanbutts@gmail.com>
2018-01-11 23:45:00 -0800

    dynamic map sizing and assoc styles

--- js/initializer.js
+++ js/initializer.js
@@ -15,3 +15,15 @@ function addCountyToMap(map) {
     }
   });
 }
+
+// We will need to replace the accessToken before releasing (since
+// you are just using mine right now)
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia3VhbmIiLCJhIjoidXdWUVZ2USJ9.qNKXXP6z9_fKA8qrmpOi6Q', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoia3VhbmIiLCJhIjoidXdWUVZ2USJ9.qNKXXP6z9_fKA8qrmpOi6Q'
+}).addTo(mainMap);
+
+// Add the county to the map
+addCountyToMap(mainMap);
