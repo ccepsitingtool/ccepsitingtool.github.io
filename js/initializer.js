d85c40cf0493be46be5ea8d5b2181c9dc9fd1913
Kuan Butts <kuanbutts@gmail.com>
2018-02-04 08:46:36 -0800

    initializer now adds two layers to the map, incl satellite

--- js/initializer.js
+++ js/initializer.js
@@ -74,11 +74,33 @@ function getLatLngCenter(latLngInDegr) {
 }
 // We will need to replace the accessToken before releasing (since
 // you are just using mine right now)
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia3VhbmIiLCJhIjoidXdWUVZ2USJ9.qNKXXP6z9_fKA8qrmpOi6Q', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+var mbAccessToken = 'pk.eyJ1Ijoia3VhbmIiLCJhIjoidXdWUVZ2USJ9.qNKXXP6z9_fKA8qrmpOi6Q';
+var leafletAccessURL = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mbAccessToken;
+var mapAttr = ('Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+  '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  'Imagery © <a href="http://mapbox.com">Mapbox</a>');
+
+var leafletLayer_Streets = L.tileLayer(leafletAccessURL, {
+  attribution: mapAttr,
   maxZoom: 18,
   id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1Ijoia3VhbmIiLCJhIjoidXdWUVZ2USJ9.qNKXXP6z9_fKA8qrmpOi6Q'
+  accessToken: mbAccessToken,
+});
+
+var leafletLayer_Satellite = L.tileLayer(leafletAccessURL, {
+  attribution: mapAttr,
+  maxZoom: 18,
+  id: 'mapbox.satellite',
+  accessToken: mbAccessToken,
+});
+
+// Add the satellite to the map as the default
+leafletLayer_Streets.addTo(mainMap);
+
+// Add the new layers to the map as a layer manager as well
+L.control.layers({
+  'Streets': leafletLayer_Streets,
+  'Satellite': leafletLayer_Satellite,
 }).addTo(mainMap);
 
 // Add geocoding search tool
