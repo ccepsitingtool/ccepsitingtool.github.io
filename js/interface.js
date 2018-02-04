3a3e1a0f291c589c5d3881b807f124f588f48a1e
michael dowd <mikdowd@gmail.com>
2018-02-04 08:33:49 -0500

    fixed download linking with new targetsiteid

--- js/interface.js
+++ js/interface.js
@@ -79,6 +79,50 @@ function printScreen() {
   }
 }
 
+
+function populateDataDownLoads() {
+
+    var voteOlItem = $('#voteCenterDl');
+    var indicatorOlItem = $('#indicatorDl');
+    var pointDlItems = [
+              ['four_day_sites.csv','Suggested Areas for 4 Day Vote Centers'],
+              ['eleven_day_sites.csv','Suggested Areas for 11 Day Vote Centers'],
+              ['dropbox_sites.csv','Suggested Areas for Ballot Drop Boxes'],
+              ['additional_sites_model.csv','Addtl. Suggested Areas for Vote Centers (Based on Model)'],
+              ['additional_sites_distance.csv','Addtl. Suggested Areas for Vote Centers (Based on Distance)'],
+              ['all_sites.csv','All Potential Areas']
+              ]
+    var miscDlItems = [
+          ["transit_stops.csv","Transit Stops"],
+          ["poi.csv","Points of Interest (All)"],
+          ["poi_misc.csv", "Points of Interest (Non-Government"],
+          ["poi_govish.csv", "Points of Interest (Government)"]
+    ]
+
+
+
+  pointDlItems.forEach(function(item){
+    flNm = item[0]
+    flLbl = item[1]
+    voteOlItem.append('<li> <a href="data/' + targetSiteId + '/point_files/' + flNm + '">' + flLbl + '</a></li>')
+  })     
+
+  var indicatorDlItem = '<li><b>Indicator Data </b> <a href="data/'
+  indicatorDlItem += targetSiteId  +  '/indicator_files/indicator_data.csv">csv</a>'
+  indicatorDlItem += ' | <a href="data/' + targetSiteId + '/indicator_files/indicator_data.zip">shp</a> </li>'
+
+  indicatorOlItem.append(indicatorDlItem);
+  miscDlItems.forEach(function(item){
+      flNm = item[0]
+      flLbl = item[1]
+      indicatorOlItem.append('<li> <a href="data/' + targetSiteId + '/point_files/' + flNm + '">' + flLbl + '</a></li>')
+    })          
+    
+
+
+}
+
 // Initializing these functions
 $('.menu-toggle').on('click', toggleMenu);
 populateNavBarIndicators();
+populateDataDownLoads();
