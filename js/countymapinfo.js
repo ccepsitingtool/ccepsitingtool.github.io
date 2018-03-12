24e555a6a4626974548e87e002d5e32488f59513
michael dowd <mikdowd@gmail.com>
2018-03-12 09:15:16 -0400

    index page text updates / link ccep logo / zoom level adjustment

--- js/countymapinfo.js
+++ js/countymapinfo.js
@@ -1,18 +1,18 @@
 var countyMapInitInfo = {
 	'081': {coord_center:[37.43,-122.340356],
-			initZoom: 10
+			initZoom: 9
 			},
 	'067': {coord_center:[38.45,-121.447258],
-			initZoom: 10
+			initZoom: 9
 			},
-	'039': {coord_center:[37.12, -119.780410],
-			initZoom: 10
+	'039': {coord_center:[37.23, -119.780410],
+			initZoom: 9
 			},
 	'057': {coord_center:[39.227266, -120.683385],
-		initZoom: 10
+		initZoom: 9
 		},
 	'055': {coord_center:[38.475915, -122.347068],
-		initZoom: 10
+		initZoom: 9
 		},
 }
 
